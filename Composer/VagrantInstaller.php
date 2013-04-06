0223bd3906d5f8f92d403ac7e56abf2196e514e7
Oleg Lobach <oleg@lobach.info>
2013-04-07 03:29:51 +0400

    rename library & make according to standard PSR-2

--- Composer/VagrantInstaller.php
+++ Composer/VagrantInstaller.php
@@ -1,21 +1,24 @@
 <?php
-namespace Zeus\Composer;
+namespace Yassa\Composer;
 
-use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
+use Composer\Package\PackageInterface;
 
-class VagrantInstaller extends LibraryInstaller {
+class VagrantInstaller extends LibraryInstaller
+{
     /**
      * {@inheritDoc}
      */
-    public function getInstallPath(PackageInterface $package) {
-        return 'dev/'. $package->getTargetDir();
+    public function getInstallPath(PackageInterface $package)
+    {
+        return 'dev/' . $package->getTargetDir();
     }
 
     /**
      * {@inheritDoc}
      */
-    public function supports($packageType) {
+    public function supports($packageType)
+    {
         return 'vagrant-env' === $packageType;
     }
 }
